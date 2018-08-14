f8ebe1150cd19c0060cf0cfcfec1e5d4799fb9be
Mike Taulty <mtaulty@microsoft.com>
2018-08-14 08:55:44 +0100

    Initial commit

---
+++ App8/App8/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using BarcodeScanning;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace App8
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        async void OnScan()
+        {
+            this.ScannedCode = "waiting...";
+
+            var newCode = await BarcodeScanningHelper.ScanOnceNoPreviewAsync(
+                TimeSpan.FromMilliseconds(-1));
+
+            if (newCode?.ScanDataLabel != null)
+            {
+                this.ScannedCode = new string(UnicodeEncoding.ASCII.GetChars(newCode.ScanDataLabel.ToArray()));
+            }
+            else
+            {
+                this.ScannedCode = null;
+            }
+        }
+        string ScannedCode
+        {
+            get => this.scannedCode;
+            set
+            {
+                this.scannedCode = value;
+                this.FirePropertyChanged();
+            }
+        }
+        string scannedCode;
+
+        void FirePropertyChanged([CallerMemberName] string property = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ App8/BarcodeScanning/BarcodeScanningHelper.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.PointOfService;
+using Windows.Foundation;
+using Windows.Media.Capture;
+using Windows.Storage;
+
+namespace BarcodeScanning
+{
+    public static class BarcodeScanningHelper
+    {
+        static string StoredBarcodeDeviceId
+        {
+            get
+            {
+                object value = null;
+
+                ApplicationData.Current.LocalSettings.Values.TryGetValue(
+                    SETTINGS_KEY, out value);
+
+                return (value as string);
+            }
+            set
+            {
+                ApplicationData.Current.LocalSettings.Values.Add(
+                    SETTINGS_KEY, value);
+            }
+        }
+        static string CachedBarcodeDeviceId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(cachedBarcodeDeviceId))
+                {
+                    cachedBarcodeDeviceId = StoredBarcodeDeviceId;
+                }
+                return (cachedBarcodeDeviceId);
+            }
+            set
+            {
+                cachedBarcodeDeviceId = value;
+
+                if (StoredBarcodeDeviceId == null)
+                {
+                    StoredBarcodeDeviceId = cachedBarcodeDeviceId;
+                }
+            }
+        }
+        static async Task<BarcodeScanner> FindBarcodeScannerAsync()
+        {
+            BarcodeScanner locatedScanner = null;
+
+            if (CachedBarcodeDeviceId == null)
+            {
+                var deviceSelector = BarcodeScanner.GetDeviceSelector();
+
+                var devices = await DeviceInformation.FindAllAsync(deviceSelector);
+
+                if (devices != null)
+                {
+                    foreach (var device in devices)
+                    {
+                        var scanner = await BarcodeScanner.FromIdAsync(device.Id);
+
+                        if (scanner.VideoDeviceId != null)
+                        {
+                            CachedBarcodeDeviceId = device.Id;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (CachedBarcodeDeviceId != null)
+            {
+                locatedScanner = await BarcodeScanner.FromIdAsync(CachedBarcodeDeviceId);
+            }
+            return (locatedScanner);
+        }
+        public static async Task<BarcodeScannerReport> ScanOnceNoPreviewAsync(
+            TimeSpan timeout)
+        {
+            var scanner = await FindBarcodeScannerAsync();
+            BarcodeScannerReport report = null;
+
+            if (scanner != null)
+            {
+                using (var capture = new MediaCapture())
+                {
+                    await capture.InitializeAsync(
+                        new MediaCaptureInitializationSettings()
+                        {
+                            VideoDeviceId = scanner.VideoDeviceId,
+                            StreamingCaptureMode = StreamingCaptureMode.Video,
+                            PhotoCaptureSource = PhotoCaptureSource.VideoPreview
+                        }
+                    );
+                    using (var claimedScanner = await scanner.ClaimScannerAsync())
+                    {
+                        // TODO: need to check all these flags
+                        claimedScanner.IsVideoPreviewShownOnEnable = false;
+                        claimedScanner.IsDecodeDataEnabled = true;
+                        claimedScanner.IsDisabledOnDataReceived = true;
+
+                        TaskCompletionSource<bool> completed = new TaskCompletionSource<bool>();
+
+                        TypedEventHandler<ClaimedBarcodeScanner, BarcodeScannerDataReceivedEventArgs> handler =
+                            (s, e) =>
+                            {
+                                report = e.Report;
+                                completed.SetResult(true);
+                            };
+
+                        claimedScanner.DataReceived += handler;
+
+                        await claimedScanner.EnableAsync();
+                        await claimedScanner.StartSoftwareTriggerAsync();
+
+                        await Task.WhenAny(completed.Task, Task.Delay(timeout));
+
+                        await claimedScanner.StopSoftwareTriggerAsync();
+
+                        claimedScanner.DataReceived -= handler;
+                    }
+                }
+            }
+            return (report);
+        }
+        static string cachedBarcodeDeviceId;
+        static readonly string SETTINGS_KEY = "CACHED_BARCODE_SCANNER_ID";
+    }
+}
